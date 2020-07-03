4c0bd547acfed88fce7ee04b4fccee28d3d7148e
Amdrei Kalianchuk <andrey.kalenchuk@gmail.com>
2020-07-03 18:05:24 -0400

    add expected data for test

---
+++ data/dotaHomePage.js
@@ -0,0 +1,19 @@
+const expectedGen = {
+    title: 'Complex Counter App',
+    header: "Counters",
+    totalResult: "Total: 0",
+    addNameField: "Counter Name",
+    addNameFieldLabel: "Enter Counter Title:",
+    defaultValueField: "50",
+    defaultValueFieldLabel: "Enter Initial Count:",
+    addCounterBtn: "ADD COUNTER",
+    LLFinput: 2,
+    ULFinput: 8,
+    totalRes: 6,
+    totalResTest: 56,
+    totalDefolt: 0,
+    totalCounters: 2,
+    counterDefault: 1,
+}
+
+export {expectedGen}
